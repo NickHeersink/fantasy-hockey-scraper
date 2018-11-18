cc7ff171c305a1a8e555705d22cc912101d767b6
Nick Heersink <nickheersink@gmail.com>
2018-11-18 18:30:09 -0500

    Add more functions

--- draft_rank.py
+++ draft_rank.py
@@ -17,29 +17,46 @@ def login(driver):
 	password.send_keys(settings.YAHOO_PASSWORD)
 	driver.find_element_by_id('login-signin').click()
 
-def get_draft_results(driver):
+def get_draft_results(driver, players, draft_order):
 	draft_order = ['bohan', 'nico', 'calvin', 'nick', 'tim', 'joel', 'jeremy', 'nate']
 	draft_order = draft_order + draft_order[::-1]
 
-	player_web_elem_list = driver.find_elements_by_class_name('name')
-	player_list = [player.text for player in player_web_elem_list]
+	players_web_elems = driver.find_elements_by_class_name('name')
+	players = [player.text for player in players_web_elems]
 
-	draft_order_list = draft_order*13
+	draft_order *= 13
 
-def make_connection():
-	driver = webdriver.Chrome()
-	driver.set_page_load_timeout(60)
+def get_player_ranks(driver, players, ranks):
+	print('hi')
+	
+def plot_draft_results(players, draft_order):
+	print('hello')
 
+# Login to the yahoo site
+def make_connection(driver):
 	login(driver)
 
-	get_draft_results(driver)
-
+# Clean up the Chrome webdriver
+def end_connection(driver):
 	driver.close()
 
 def main():
 	print('Welcome to the Non-Competitive Action League')
 	
-	make_connection()
+	driver = webdriver.Chrome()
+	driver.set_page_load_timeout(60)
+	make_connection(driver)
+
+	players = []
+	ranks = []
+	draft_order = []
+
+	get_draft_results(driver, players, draft_order)
+	get_player_ranks(driver, players, ranks)
+	
+	end_connection(driver)
+
+	plot_draft_results(players, draft_order)
 
 
 if __name__ == "__main__":
