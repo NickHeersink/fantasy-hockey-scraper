3757d220338001bb233f7d9a9d173bdcce85f11e
Nick Heersink <nickheersink@gmail.com>
2018-11-19 22:34:48 -0500

    Add pickle ability to store/load data

--- draft_rank.py
+++ draft_rank.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
+import pickle
 import settings
 import time
-
 
 def login(driver):
 	driver.get(settings.YAHOO_URL)
@@ -26,13 +26,32 @@ def get_draft_results(driver, players, draft_order):
 
 	draft_order *= 13
 
+	return players, draft_order
+
 def get_player_ranks(driver, players, ranks):
 	print('hi')
 	
+# Store info in .dat file
+def store_info(players, draft_order, ranks):
+	with open('players.pickle', 'wb') as file:
+		pickle.dump(players, file)
+
+	with open('draft_order.pickle', 'wb') as file:
+		pickle.dump(draft_order, file)
+
+	with open('ranks.pickle', 'wb') as file:
+		pickle.dump(ranks, file)
+
+# Retrieve stored info from .dat file
+def get_stored_info(players, draft_order, ranks):
+	file = open('players.pickle', 'rb')		
+	players = pickle.load(file)
+
+# Plot the results
 def plot_draft_results(players, draft_order):
 	print('hello')
 
-# Login to the yahoo site
+# Login to the Yahoo site
 def make_connection(driver):
 	login(driver)
 
@@ -42,19 +61,27 @@ def end_connection(driver):
 
 def main():
 	print('Welcome to the Non-Competitive Action League')
-	
-	driver = webdriver.Chrome()
-	driver.set_page_load_timeout(60)
-	make_connection(driver)
+
+	# Set this to pull new data
+	need_new_data = False
 
 	players = []
 	ranks = []
 	draft_order = []
 
-	get_draft_results(driver, players, draft_order)
-	get_player_ranks(driver, players, ranks)
+	if need_new_data:
+		driver = webdriver.Chrome()
+		driver.set_page_load_timeout(60)
+		make_connection(driver)
+
+		players, draft_order = get_draft_results(driver, players, draft_order)
+		ranks = get_player_ranks(driver, players, ranks)
+
+		store_info(players, draft_order, ranks)
 	
-	end_connection(driver)
+		end_connection(driver)
+	else:
+		get_stored_info(players, draft_order, ranks)
 
 	plot_draft_results(players, draft_order)
 
